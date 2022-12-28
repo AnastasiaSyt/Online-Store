b202a0cd1be6a1554c95a0a894e111a91b9bb051
Anastasia Sytsevich <drozdushka@gmail.com>
2022-12-28 16:38:12 +0300

    feat: add routes and class App

---
+++ src/components/app/app.ts
@@ -0,0 +1,65 @@
+import CardProduct from "../cardProductPage/cardProductPage";
+import ErrorPage from "../error404/errorPage";
+import FilterPage from "../filterPage/filterPage";
+import { IPage } from "../IPage";
+import { PageIDs } from "../types";
+
+export default class App {
+    container: HTMLElement;
+
+    constructor(content: HTMLElement) {
+        this.container = content;
+        this.changeRouteHandler();
+        this.run();
+    }
+    
+    renderNewPage(id: PageIDs) {
+        this.container?.childNodes.forEach((node) => this.container.removeChild(node));
+        let newPage: IPage | null = null; 
+
+        if (id === PageIDs.FilterPage) {
+            newPage = new FilterPage();
+        } else if (id === PageIDs.CardProductPage) {
+            newPage = new CardProduct();
+        } else if (id === PageIDs.CartPage) {
+            console.log('Hello');
+        } else if (id === PageIDs.ErrorPage) {
+            newPage = new ErrorPage();
+        }
+
+        if (newPage) {
+            const pageHTML = newPage.getPage();
+            this.container.append(pageHTML);
+        }
+    }
+
+    changeRouteHandler() {
+        window.addEventListener('hashchange', () => {
+            const hash = window.location.hash.slice(1);
+            console.log(hash);
+            this.renderNewPage(hash as PageIDs);
+        })
+    }
+
+    run() {
+        let hash = window.location.hash.slice(1) as PageIDs;
+        if (hash !== PageIDs.CardProductPage &&
+            hash !== PageIDs.CartPage &&
+            hash !== PageIDs.FilterPage
+        ) {
+            this.setLocation(PageIDs.ErrorPage);
+        } else {
+            this.renderNewPage(hash);
+        }
+    }
+
+    setLocation(page: PageIDs) {
+        try {
+          history.pushState(null, '', page);
+          return;
+        } catch(e) {
+            console.log(e);
+        }
+        location.hash = `#${page}`;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,23 @@ import FilterPage from "./components/filterPage/filterPage";
 import CardProduct from "./components/cardProductPage/cardProductPage";
 import BasketPage from "./components/basket/basket";
 import './style.css';
+import App from "./components/app/app";
 
 
+// const content = document.getElementById("content");
+// content?.childNodes.forEach((node) => content.removeChild(node));
+// // content?.append(new FilterPage().getPage());
+
+// const card = new CardProduct();
+// const cardProduct = card.getPage();
+// card.addItems();
+
+// content?.append(cardProduct);
 
 const content = document.getElementById("content");
-content?.childNodes.forEach((node) => content.removeChild(node));
 
- content?.append(new FilterPage().getPage());
+if (content) {
+    const app = new App(content);
+    app.run();
+}
 
-const card = new CardProduct();
-const cardProduct = card.getCardProduct();
-card.addItems();
-content?.append(cardProduct);
-content?.append(new BasketPage().getPage());
