bff4b11964c7839cce1e7b4e490b67e8ac74f602
Evgeniy <jdev03sgo@gmail.com>
2023-01-09 19:00:05 +0300

    add: adder for card button

--- src/components/filterPage/card.ts
+++ src/components/filterPage/card.ts
@@ -32,7 +32,27 @@ export default class Card implements ICard{
         const node = document.createElement(config.tag);
         config.classes.forEach((className) => {
             node.classList.add(className);
+            if(className === 'text_tag'){
+                if(localStorage.getItem('basketFlowers')){
+                    let tempFlowers = JSON.parse(localStorage.getItem('basketFlowers') ?? '');
+                    if(tempFlowers){
+                        tempFlowers.push(Number(window.location.href.split('_')[1]));
+                        const tempSet = new Set(tempFlowers);
+                        tempFlowers = Array.from(tempSet);
+                        document.querySelector('.count')!.textContent = tempFlowers.length;
+                        localStorage.setItem('basketFlowers', JSON.stringify(tempFlowers))
+
+                    }
+                }
+                else{
+                    const basketFlowers = [];
+                    basketFlowers.push(Number(window.location.href.split('_')[1]));
+                    localStorage.setItem('basketFlowers', JSON.stringify(basketFlowers))
+                    document.querySelector('.count')!.textContent = '1';
+                }
+            }
         });
+
         if (config.label) {
             node.textContent = config.label;
         }
